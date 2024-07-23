c75d3e6d458ac4a407210237601c161f89f1d40b
grisak4 <grisak4@gmail.com>
2024-07-23 15:23:00 +0700

    написал базовый код, который нужно отредачить

---
+++ server/database/db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"auth-test/config"
+	"database/sql"
+	"fmt"
+	"log"
+
+	//"C:\Users\grigo\Documents\pet projects\authServ\server\config"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func ConnectDB() {
+	var err error
+	connConf := config.ReadConfigDatabase()
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		connConf.User, connConf.Password, connConf.Host, connConf.Port, connConf.DatabaseName)
+
+	// Подключение к базе данных
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %s", err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+}
+
+func InitDb() *sql.DB {
+	return db
+}
+
+func CloseDB() {
+	defer db.Close()
+}
