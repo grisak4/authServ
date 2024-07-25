fa0241f56cd819339b49e5df84821d024c0aae9f
Karkar1ch <karina.rogalevich@email.com>
2024-07-25 17:51:25 +0200

    начала делать авторизацию, пока не работает

--- client/js/script.js
+++ client/js/script.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Переключение на окно регистрации
     document.getElementById("toggleSignup").addEventListener("click", function(event) {
-        event.preventDefault(); // Предотвращаем переход по ссылке
+        event.preventDefault(); 
         document.querySelector(".window_login").classList.remove("active");
         document.querySelector(".window_signup").classList.add("active");
     });
 
-    // Переключение на окно входа
     document.getElementById("toggleLogin").addEventListener("click", function(event) {
-        event.preventDefault(); // Предотвращаем переход по ссылке
+        event.preventDefault(); 
         document.querySelector(".window_signup").classList.remove("active");
         document.querySelector(".window_login").classList.add("active");
     });
